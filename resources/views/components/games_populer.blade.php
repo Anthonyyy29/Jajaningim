40667cddd23f6d0367ebefad992991f2ee867b5d
ezra <zra@ezras-MacBook-Air.local>
2026-06-25 13:50:59 +0700

    itu pokoknya

---
+++ resources/views/components/games_populer.blade.php
@@ -0,0 +1,24 @@
+<div class="jumbotron text-left mt-4">
+    <h2>POPULER</h2>
+
+    <div class="container">
+        <div class="container text-center">
+            @isset($games)
+                <div class="row g-3">
+                    @foreach ($games as $game)
+                        <div class="col-6 col-md-4 col-lg-2">
+                            <div class="text-center p-3 rounded-4 promo_banner h-100">
+                                <a href="{{ route('games', $game['id']) }}">
+                                    <img src="{{ asset($game['logo']) }}" alt="{{ $game['name'] }}" class="img-fluid mb-2" style="max-height: 100px;">
+                                </a>
+                                <div class="small fw-semibold">{{ $game['name'] }}</div>
+                            </div>
+                        </div>
+                    @endforeach
+                </div>
+            @else
+                <p>Belum ada game populer untuk ditampilkan.</p>
+            @endisset
+        </div>
+    </div>
+</div>
