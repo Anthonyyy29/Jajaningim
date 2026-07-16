57f6773115bd0a3fc3cd6c6f295d72bb79408c90
ezra <zra@ezras-MacBook-Air.local>
2026-07-16 13:56:20 +0700

    anuan

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -5,6 +5,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>{{ config('app.name', 'Jajaningim') }}</title>
 
+    <link rel="icon" type="image/png" href="{{ asset('assets/logo_jajaningim/logo.png') }}">
+    <link rel="apple-touch-icon" href="{{ asset('assets/logo_jajaningim/logo.png') }}">
+
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link href="https://fonts.googleapis.com/css2?family=Saira+Stencil+One&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.css">
